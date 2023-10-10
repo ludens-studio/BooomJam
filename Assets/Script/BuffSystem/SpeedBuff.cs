f2520c6987cd3c4967dba7adbd8be3c3fcf7d100
Hongyi.Lan <18965093000@163.com>
2023-10-10 20:35:17 +0800

    Update SpeedBuff.cs

--- Assets/Script/BuffSystem/SpeedBuff.cs
+++ Assets/Script/BuffSystem/SpeedBuff.cs
@@ -8,24 +8,35 @@ public class SpeedBuff : Buff
     public float amount;
     [SerializeField] private float amount_real; //实际的调整数值
     private float save_speed; 
+
+    private bool isUse = false ;
     public override void EnterBuff()
     {
         save_speed = target.GetComponent<Obj>().speed; 
 
-        float tmp = 0; 
-        tmp = target.speed + amount;
-        if(tmp < 0)
-        {
-            // 超出最低速度
-            amount_real = target.speed - 0; //存储最大能够修改的数值
+        if(target.GetComponent<Obj>().CheckBuffCount("Speed_down") <= 1) {
+            float tmp = 0;
+            tmp = target.speed + amount;
+            if (tmp < 0)
+            {
+                // 超出最低速度
+                amount_real = target.speed - 0; //存储最大能够修改的数值
+            }
+            else
+            {
+                // 没有超出最低速度的话，amount_real就是amount
+                amount_real = amount;
+            }
+
+            target.speed = target.speed + amount_real;
+            isUse= true;
         }
         else
         {
-            // 没有超出最低速度的话，amount_real就是amount
-            amount_real = amount; 
+            isUse = false;
         }
+       
 
-        target.speed = target.speed + amount_real;
     }
 
     public override void UseBuff()
@@ -35,10 +46,15 @@ public class SpeedBuff : Buff
 
     public override void ExitBuff()
     {
-        target.speed -= amount_real;
-        if(target.speed < 0 || target.speed > save_speed) {
-            target.speed = target.save_speed; 
+        if (isUse)
+        {
+            target.speed -= amount_real;
+            if (target.speed < 0 || target.speed > save_speed)
+            {
+                target.speed = target.save_speed;
+            }
         }
+
     }
 
 
