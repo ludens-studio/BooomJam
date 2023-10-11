cb4a1f634b67dcb39b09f2e20e278fdd27b95128
Guiny-Time <1084926982@qq.com>
2023-10-11 16:49:31 +0800

    分辨率适配

--- Assets/Script/CameraMgr.cs
+++ Assets/Script/CameraMgr.cs
@@ -1,17 +1,18 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class CameraMgr : BaseMgr<CameraMgr>
 {
     public GameObject guideWindow;
+    public Vector3 worldPos1920;
 
-    public bool isShake; //没必要同时发生多个震动
-    public float shakeTime;
-    public float shakeStrength;
-
-    public float defaultDuration;
-    public float defaultStrength;
+    private void Start()
+    {
+        transform.position = worldPos1920;
+    }
 
     public void PauseGame()
     {
@@ -19,36 +20,4 @@ public class CameraMgr : BaseMgr<CameraMgr>
         Time.timeScale = 0;
     }
 
-
-    public void ShakeCamera()
-    {
-        if (!isShake)
-        {
-            StartCoroutine(Shake(defaultDuration, defaultStrength));
-        }
-    }
-    public void ShakeCamera(float _duration, float _strength)
-    {
-        if (!isShake)
-        {
-            StartCoroutine(Shake(_duration, _strength));
-        }
-
-    }
-
-    IEnumerator Shake(float _duration, float _strength)
-    {
-        isShake = true;
-        Vector3 startPosition = this.transform.position;
-
-        while (_duration > 0)
-        {
-            this.transform.position = Random.insideUnitSphere * _strength + startPosition;
-            _duration -= Time.deltaTime;
-            yield return null;
-        }
-
-        this.transform.position = startPosition;
-        isShake = false;
-    }
 }
